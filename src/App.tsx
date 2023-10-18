d24ffbcb100c463e74ced178377cedc027c82f11
Sabry Awad <dr.sabry1997@gmail.com>
2023-10-18 16:11:32 +0300

    ✨ feat(package.json): Add "react-icons" dependency 📝 Summary: Added the "react-icons" dependency to the "dependencies" section in the package.json file. 💡 Details: - The "react-icons" library is essential for utilizing icons within the React application. - By including this dependency, the project gains access to a wide variety of icons for use in the user interface. ➕ Dependencies: - `"react-icons": "^4.11.0"` 🔍 Context: - To enhance the project's user interface and allow for the incorporation of icons within the application, the "react-icons" dependency has been introduced. 📚 Challenges: - N/A ✍️ Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>
    ✨ feat(App.tsx): Add Twitter icon to App component
    📝 Summary:
    Integrated a Twitter icon from the "react-icons" library into the App component, allowing users to easily share content on Twitter.
    💡 Details:
    - Imported the "FaTwitter" icon from "react-icons/fa" and added it to the component.
    - Created a button with a Twitter icon to facilitate sharing content on Twitter.
    🔍 Context:
    - The addition of a Twitter sharing feature is expected to enhance the user experience and increase social media engagement with the application.
    📚 Challenges:
    - N/A
    ✍️ Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import './App.css';
+import { FaTwitter } from 'react-icons/fa';
 
-function App() {
-  return <>Hello world!</>;
-}
+const App = () => {
+  return (
+    <div className="quote-container">
+      <div className="button-container">
+        <button className="twitter-button" title="Tweet This!">
+          <FaTwitter />
+        </button>
+      </div>
+    </div>
+  );
+};
 
 export default App;
