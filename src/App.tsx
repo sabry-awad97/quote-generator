f44dd72de860c0218d8ca4d127a6b6270da1f75b
Sabry Awad <dr.sabry1997@gmail.com>
2023-10-18 16:36:02 +0300

    📦 feat(package.json): Add axios dependency 🚀 feat(src/App.tsx): Integrate axios for fetching quotes 📝 Summary: Added the axios package as a dependency in package.json. In src/App.tsx, integrated axios for fetching quotes from an API, and implemented cancellation of requests when the component is unmounted. 💡 Details: - In package.json, added the axios package as a dependency with version "^1.5.1". - In src/App.tsx, implemented fetching of quotes using axios, along with cancellation of requests when the component is unmounted. - Used the CancelToken feature of axios to handle request cancellation. ➕ Dependencies: - Added "axios": "^1.5.1" to the dependencies in package.json. 🔍 Context: - Axios was introduced to facilitate HTTP requests for fetching quotes from an external API. - Request cancellation was implemented to prevent unnecessary API calls when the component is no longer in use. 📚 Challenges: - N/A ✍️ Signed-off-by: Sabry Awad <dr.sabry1997@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,43 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import { FaTwitter } from 'react-icons/fa';
+import axios, { isCancel } from 'axios';
+
+interface IQuote {
+  text: string;
+  author: string | null;
+}
 
 const App = () => {
+  const [quotes, setQuotes] = useState<IQuote[]>([]);
+  const source = axios.CancelToken.source();
+
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      try {
+        const { data } = await axios.get<IQuote[]>(
+          'https://type.fit/api/quotes',
+          {
+            cancelToken: source.token,
+          }
+        );
+        setQuotes(data);
+      } catch (error) {
+        if (isCancel(error)) {
+          console.log('Request canceled:', error.message);
+        } else {
+          throw error;
+        }
+      }
+    };
+
+    fetchQuotes();
+
+    return () => {
+      source.cancel('Component is unmounted');
+    };
+  }, [source]);
+
   return (
     <div className="quote-container">
       <div className="button-container">
